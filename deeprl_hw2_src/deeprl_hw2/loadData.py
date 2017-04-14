46aa70e178668eacc64b9c42d13db9db601a6394
Tejas Nagendra <tejasnagendra@Tejas.wv.cc.cmu.edu>
2017-04-14 00:35:14 -0400

    split into train and test sets

--- deeprl_hw2_src/deeprl_hw2/loadData.py
+++ deeprl_hw2_src/deeprl_hw2/loadData.py
@@ -5,15 +5,21 @@ class DataSet():
     def __init__(self, path):
         self.path = path
 
-    def load(self):
+    def load(self, trainSplit):
         fp = open(self.path, "r")
         data = json.load(fp)
-        list_data = []
+        self.list_data = []
         for key, value in data.iteritems():
-            list_data.append((value['file_name'].rsplit('_',1)[0] + '.jpg', value['sent'], value['bbox']))
-        self.list_data = list_data
+            self.list_data.append((value['file_name'].rsplit('_',1)[0] + '.jpg', value['sent'], value['bbox'], value['sent_repr']))
+
+        shuffle(self.list_data)
+        train_len = int(trainSplit*len(self.list_data))
+        self.train_data = self.list_data[:train_len]
+        self.test_data = self.list_data[train_len:]
 
     def permute(self):
-        shuffle(self.list_data)
+        shuffle(self.train_data)
+
+
 
 
